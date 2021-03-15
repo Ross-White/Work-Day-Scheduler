610afe4ec9175decf1aa79dac67b2eef722d87c0
Ross White <rosswhite@outlook.com>
2021-03-15 15:50:56 +0000

    Time Block elements added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-//Display Time function
+let timeBlocksArr = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+
+
+//Display date function
 function displayDate () {
-    let date = moment().format('MMMM Do YYYY');
+    let date = moment().format('dddd MMMM Do ');
     $('#currentDay').text(date);
 }
+
+for (let i = 1; i < timeBlocksArr.length; i++) {
+    let newTimeBlockEL= $("#9am").clone();
+    newTimeBlockEL.attr("id", timeBlocksArr[i]); 
+    newTimeBlockEL.children(".row").attr("style", "white-space: pre-Wrap"); 
+    newTimeBlockEL.children(".row").children(".hour").text(timeBlocksArr[i]); 
+    newTimeBlockEL.appendTo(".container"); 
+
+}; 
+
 
 setInterval(displayDate, 1000);
